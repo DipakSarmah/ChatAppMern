b448e38340647ff26db7ae37eca6ab57aeed9f15
DIPAK SARMAH <92313801+DipakSarmah@users.noreply.github.com>
2024-01-29 16:29:28 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,12 +12,6 @@ app.use(cors());
 app.use('/users', userRoutes);
 require('./connection')
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://chat-app-frontend-sepia-one.vercel.app');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
 const rooms = ['general', 'tech', 'finance', 'crypto'];
 
 const server = require('http').createServer(app);
@@ -26,6 +20,7 @@ const io = require('socket.io')(server,{
     cors: {
         origin: 'https://chat-app-frontend-sepia-one.vercel.app',
         methods: ['GET','POST'],
+        credentials: true,
     }
 })
 
