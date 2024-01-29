803bf5661820ce3701739aef3b7dc9499e4ba631
DIPAK SARMAH <92313801+DipakSarmah@users.noreply.github.com>
2024-01-29 16:00:36 +0530

    Update appApi.js

--- frontend/src/services/appApi.js
+++ frontend/src/services/appApi.js
@@ -4,7 +4,7 @@ import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 const appApi = createApi({
     reducerPath: 'appApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'https://chat-app-mern-blond.vercel.app/'
+        baseUrl: 'https://chat-app-mern-blond.vercel.app'
     }),
 
     endpoints: (builder)=>({
